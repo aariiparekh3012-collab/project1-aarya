74e9446412c5102e1f4638971a9471d5200025bf
Aarya Parekh <aariiparekh3012@gmail.com>
2026-08-02 21:26:24 +0530

    EDA complete - NTPC data + 6 plots

--- testfile1.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-import pandas as pd
-import yfinance as yf
-
-print("Setup is working.")
-print("Python version:", sys.version)
-print("Pandas version:", pd.__version__)
-print("YFinance imported successfully.")
